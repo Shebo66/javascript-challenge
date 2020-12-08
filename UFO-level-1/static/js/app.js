d3850ef23f919e3c6dfe874b043a6bbf342b1c6a
Shebo66 <70512412+Shebo66@users.noreply.github.com>
2020-12-08 23:57:28 +0800

    javascript3

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,30 @@
+var tableData = data
+// object is from {}
+var tbody=d3.select('tbody')
+data.forEach ((row)=>{
+    // we will loop through every key value in each 
+    // tr is an html element meeaning row
+    var cells = tbody.append('tr')
+    Object.values(row).forEach((value)=>{
+        // td is a cell
+    cells.append('td').text(value)
+    })
+})
+function buttonclick(){
+    // html has different properties
+    // 'value' is in quotes as a string
+    tbody.html('')
+var date = d3.selectAll('#datetime').property('value')
+filterData = tableData.filter(obj=>obj.datetime==date)
+filterData.forEach ((row)=>{
+    // we will loop through every key value in each 
+    // tr is an html element meeaning row
+    var cells = tbody.append('tr')
+    Object.values(row).forEach((value)=>{
+        // td is a cell
+    cells.append('td').text(value)
+    })
+})
+}
+// button click is a funtion
+d3.selectAll('#filter-btn').on('click',buttonclick)
