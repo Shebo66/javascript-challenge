a001a808e9c0dd5c0c8f43fc66712482059af3a1
Shebo66 <70512412+Shebo66@users.noreply.github.com>
2020-12-15 02:22:19 +0800

    javascriptfinal

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,8 +2,8 @@ var tableData = data
 // object is from {}
 var tbody=d3.select('tbody')
 data.forEach ((row)=>{
-    // we will loop through every key value in each 
-    // tr is an html element meeaning row
+    // we will loop through every key value in each ...
+    // tr is an html element meaning row
     var cells = tbody.append('tr')
     Object.values(row).forEach((value)=>{
         // td is a cell
@@ -17,8 +17,8 @@ function buttonclick(){
 var date = d3.selectAll('#datetime').property('value')
 filterData = tableData.filter(obj=>obj.datetime==date)
 filterData.forEach ((row)=>{
-    // we will loop through every key value in each 
-    // tr is an html element meeaning row
+    // we will loop through every key value in each ...
+    // tr is an html element meaning row
     var cells = tbody.append('tr')
     Object.values(row).forEach((value)=>{
         // td is a cell
@@ -26,5 +26,5 @@ filterData.forEach ((row)=>{
     })
 })
 }
-// button click is a funtion
+// button click is a funtion. Refer to line 44 in html id=filter-btn
 d3.selectAll('#filter-btn').on('click',buttonclick)
